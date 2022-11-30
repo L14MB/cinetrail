d213910cb9aa974b0cc5b22c02cfae39cf3f3de1
L14MB <liam6914@outlook.com>
2022-11-30 20:09:19 +0000

    created the header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import ThemeContextProvider from './contexts/ThemeContext';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeContextProvider>
+        <Header />
+      </ThemeContextProvider>
+    </BrowserRouter>  
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React,{useContext} from 'react'
+import '../styles/header.css'
+import {Link} from 'react-router-dom'
+import { MdOutlineDarkMode,MdOutlineLightMode } from 'react-icons/md';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+
+function Header() {
+
+const {darkMode,setDarkMode} = useContext(ThemeContext)
+
+
+const handleTheme =  () => {
+    setDarkMode(!darkMode)
+    localStorage.setItem('darkMode',!darkMode)
+}
+
+    return (
+        <div className={darkMode ? 'header-container' : 'header-container header-light'}>
+            <Link className='logo' to="/">CineTrail</Link>
+            <div className='search-container'>
+                <input className='search-input' placeholder='Search movies...'/>
+        </div>
+        <div className='header-buttons-container'>
+            <div className='theme-button-container'>
+
+                {
+                    darkMode
+                    ? <div className='theme-buttons'>
+                        <MdOutlineLightMode onClick={handleTheme} className='theme-icon'/>
+                        <MdOutlineDarkMode className='theme-icon theme-icon-active'/>
+                      </div>
+                    : <div className='theme-buttons'>
+                        <MdOutlineLightMode className='theme-icon theme-icon-active'/>
+                        <MdOutlineDarkMode onClick={handleTheme} className='theme-icon'/>
+                      </div>
+                }
+            </div>
+            <div>
+                <button className='create-account-btn'>Create an account</button>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Header
